9ea18d468754e7dc637b835d23b50774e241b2e8
Mpho Letshwenyo <108421871+girlxcodee@users.noreply.github.com>
2023-03-12 17:44:52 +0200

    First commit

---
+++ Assignment 4/Assets/Scipts/RepeatingBckground.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBckground : MonoBehaviour
+{
+
+    public GameObject camera;
+    public float parallaxEffect;
+    private float width, positionX;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
+        positionX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float parallaxDistance = camera.transform.position.x * parallaxEffect;
+        float remainingDistance = camera.transform.position.x * (1 - parallaxEffect);
+
+        transform.position = new Vector3(positionX + parallaxDistance, transform.position.y, transform.position.z);
+
+        if (remainingDistance > positionX + width)
+        {
+            positionX += width;
+        }
+    }
+}
